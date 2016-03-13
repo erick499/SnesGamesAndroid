e7133b863c4eadf4d1bfd7a7c195fe97242bce09
yagoazedias <yagoluis9@hotmail.com>
2016-03-13 15:47:30 -0300

    Version 0.65

--- app/src/main/java/io/github/yagoazedias/snesgamesandroid/Car.java
+++ app/src/main/java/io/github/yagoazedias/snesgamesandroid/Car.java
@@ -1,4 +1,13 @@
 package io.github.yagoazedias.snesgamesandroid;
+import android.graphics.Paint;
 
 public class Car {
+
+    int x = 20;
+    int y = 20;
+    int height = y + 129;
+    int width  = x + 229;
+
+    Paint paint = new Paint();
+
 }
--- app/src/main/java/io/github/yagoazedias/snesgamesandroid/CustomView.java
+++ app/src/main/java/io/github/yagoazedias/snesgamesandroid/CustomView.java
@@ -16,11 +16,18 @@ public class CustomView extends View implements Runnable {
     public Plant[] plants2 = new Plant[3];
     public Tree[]  trees1  = new Tree[2];
     public Tree    tree2   = new Tree();
-    public int[]   waterObjPos = {20, 169, 318, 477};
+    public int[]   LineObjPos = { 20, 169, 318, 477, 646, 815, 1010 };
+    public Car[]   carsLine1 = new Car[3];
+    public Car[]   carsLine2 = new Car[3];
+    public Car[]   carsLine3 = new Car[3];
+    public Car[]   carsLine4 = new Car[3];
+    public Car[]   carsLine5 = new Car[3];
+    public Car[]   carsLine6 = new Car[3];
 
     public void OnStart(){
         InitFlog();
         InitSurfaces();
+        InitCars();
     }
 
     public CustomView(Context context) {
@@ -35,23 +42,58 @@ public class CustomView extends View implements Runnable {
         super(context, attrs, defStyle);
     }
 
+    public void InitCars() {
+        for (int i = 0; i < carsLine1.length; i++) {
+            carsLine1[i] = new Car();
+
+            if(i == 0) {
+                carsLine1[i].x = 20;
+                carsLine1[i].y = LineObjPos[4];
+                carsLine1[i].width  = carsLine1[i].x + 229;
+                carsLine1[i].height = carsLine1[i].y + 129;
+            }else{
+                carsLine1[i].x = carsLine1[i - 1].width + 400;
+                carsLine1[i].y = LineObjPos[4];
+                carsLine1[i].width  = carsLine1[i].x + 229;
+                carsLine1[i].height = carsLine1[i].y + 129;
+            }
+        }
+    }
+
+    public void UpdateCarsLength() {
+        for (int i = 0; i < carsLine1.length; i++) {
+                carsLine1[i].width  = carsLine1[i].x + 229;
+                carsLine1[i].height = carsLine1[i].y + 129;
+        }
+    }
+
+    public void UpdateCars(){
+        for (int i = 0; i < carsLine1.length; i++) {
+            carsLine1[i].x -= 7.5;
+
+            if(carsLine1[i].x < -229){
+                carsLine1[i].x = getMeasuredWidth();
+            }
+        }
+    }
+
     public void InitSurfaces(){
         for(int i = 0; i < plants1.length; i++) {
             plants1[i] = new Plant();
 
             if(i == 0) {
                 plants1[i].x = 20;
-                plants1[i].y = waterObjPos[0];
+                plants1[i].y = LineObjPos[0];
                 plants1[i].height = plants1[i].y + 139;
                 plants1[i].width  = plants1[i].x + 139;
             }else{
                 plants1[i].x = plants1[i - 1].x + 400;
-                plants1[i].y = waterObjPos[0];
+                plants1[i].y = LineObjPos[0];
                 plants1[i].height = plants1[i].y + 139;
                 plants1[i].width  = plants1[i].x + 139;
             }
 
-            tree2.y = waterObjPos[3];
+            tree2.y = LineObjPos[3];
         }
 
         for(int i = 0; i < plants2.length; i ++) {
@@ -59,12 +101,12 @@ public class CustomView extends View implements Runnable {
 
             if(i == 0) {
                 plants2[i].x = 20;
-                plants2[i].y = waterObjPos[1];
+                plants2[i].y = LineObjPos[1];
                 plants2[i].height = plants2[i].y + 139;
                 plants2[i].width  = plants2[i].x + 139;
             }else{
                 plants2[i].x = plants2[i - 1].x + 400;
-                plants2[i].y = waterObjPos[1];
+                plants2[i].y = LineObjPos[1];
                 plants2[i].height = plants2[i].y + 139;
                 plants2[i].width  = plants2[i].x + 139;
             }
@@ -77,13 +119,13 @@ public class CustomView extends View implements Runnable {
             {
                 case 0:
                     trees1[i].x = 20;
-                    trees1[i].y = waterObjPos[2];
+                    trees1[i].y = LineObjPos[2];
                     trees1[i].width = trees1[i].x + 429;
                     trees1[i].height = trees1[i].y + 129;
                     break;
                 default:
                     trees1[i].x = trees1[i -1].x + 729;
-                    trees1[i].y = waterObjPos[2];
+                    trees1[i].y = LineObjPos[2];
                     trees1[i].width = trees1[i].x + 429;
                     trees1[i].height = trees1[i].y + 129;
                     break;
@@ -93,12 +135,12 @@ public class CustomView extends View implements Runnable {
         tree2 = new Tree();
 
         tree2.x = 450;
-        tree2.y = waterObjPos[3];
+        tree2.y = LineObjPos[3];
         tree2.width  = tree2.x + 429;
         tree2.height = tree2.y + 129;
     }
 
-    public void InitSurfacesLengths() {
+    public void UpdateSurfacesLengths() {
         for(int i = 0; i < plants1.length; i++) {
 
             if(i == 0) {
@@ -137,10 +179,7 @@ public class CustomView extends View implements Runnable {
     public void InitFlog() {
         //flog.x = 450;
         flog.y = 1749 - ((1749 / 11) / 2);
-
-        System.out.println(getMeasuredWidth());
-
-        flog.width  = flog.x + 100;
+          flog.width  = flog.x + 100;
         flog.height = flog.y + 50;
     }
 
@@ -165,7 +204,7 @@ public class CustomView extends View implements Runnable {
 
             if(plants1[i].x < -159) {
                 plants1[i].x = getMeasuredWidth();
-                InitSurfacesLengths();
+                UpdateSurfacesLengths();
             }
         }
 
@@ -174,7 +213,7 @@ public class CustomView extends View implements Runnable {
 
             if(plants2[i].x > getMeasuredWidth()) {
                 plants2[i].x = -159;
-                InitSurfacesLengths();
+                UpdateSurfacesLengths();
             }
         }
     }
@@ -185,7 +224,7 @@ public class CustomView extends View implements Runnable {
 
             if(trees1[i].x < -429) {
                 trees1[i].x = getMeasuredWidth();
-                InitSurfacesLengths();
+                UpdateSurfacesLengths();
             }
         }
 
@@ -246,6 +285,9 @@ public class CustomView extends View implements Runnable {
         for(int i = 0; i < trees1.length; i++) {
             trees1[i].paint.setColor(Color.RED);
         }
+        for(int i = 0; i < carsLine1.length; i++){
+            carsLine1[i].paint.setColor(Color.CYAN);
+        }
 
             canvas.drawRect(water.x, water.y, water.width, water.height, water.paint);
 
@@ -259,6 +301,10 @@ public class CustomView extends View implements Runnable {
 
         for(int i = 0; i < trees1.length; i++) {
             canvas.drawRect(trees1[i].x, trees1[i].y, trees1[i].width, trees1[i].height, trees1[i].paint);
+        }
+
+        for(int i = 0; i < carsLine1.length; i++){
+            canvas.drawRect(carsLine1[i].x, carsLine1[i].y, carsLine1[i].width, carsLine1[i].height, carsLine1[i].paint);
         }
 
         canvas.drawRect(tree2.x, tree2.y, tree2.width, tree2.height, tree2.paint);
@@ -277,7 +323,9 @@ public class CustomView extends View implements Runnable {
         UpdateFlogProprietes();
         UpdatePlants();
         UpdateTrees();
-        InitSurfacesLengths();
+        UpdateSurfacesLengths();
+        UpdateCarsLength();
+        UpdateCars();
     }
 
     @Override
