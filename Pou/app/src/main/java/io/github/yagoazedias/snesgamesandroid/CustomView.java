772ddaf8499753a97f1e633142fbe77cbd20a781
yagoazedias <yagoazedias@gmail.com>
2016-07-13 08:40:00 -0300

    Create board object

--- Pou/app/src/main/java/io/github/yagoazedias/snesgamesandroid/CustomView.java
+++ Pou/app/src/main/java/io/github/yagoazedias/snesgamesandroid/CustomView.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -18,10 +19,21 @@ public class CustomView extends View implements Runnable {
     float ypad = (float)(getPaddingTop() + getPaddingBottom()) + 1300;
 
     // Water properties.
-    Rect whate = new Rect(getPaddingLeft(), getPaddingBottom() +  400, getPaddingLeft() + 5000, 100);
+    Rect whate = new Rect(getPaddingLeft(), (int)ypad + 300, getPaddingLeft() + 5000, (int)ypad + 2000);
+
+    Board[] board = new Board[4];
 
     public void OnStart(){
+        // Init board positions.
+        for(int i = 0; i < board.length; i++) {
+            board[i] = new Board();
+            board[i].ypad = ypad + 100;
 
+            switch (i) {
+                case 0:  board[i].xpad = xpad - 100; break;
+                default: board[i].xpad = board[i - 1].xpad + 300; break;
+            }
+        }
     }
 
     public CustomView(Context context) {
@@ -45,6 +57,10 @@ public class CustomView extends View implements Runnable {
         super.onDraw(canvas);
         canvas.drawCircle(xpad, ypad, 100, new Paint(Paint.ANTI_ALIAS_FLAG));
         canvas.drawRect(whate, new Paint(Paint.DEV_KERN_TEXT_FLAG));
+
+        for(int i = 0; i < board.length; i++) {
+            canvas.drawRect(board[i].xpad, board[i].ypad, board[i].xpad + 200, board[i].ypad + 200, new Paint(Paint.ANTI_ALIAS_FLAG));
+        }
     }
 
     private void Update() {
