3b7691ec9d5ec2161b60a8ca882d3a9896ad644b
ErickTinocoOliveira <erick.t.oliveira@gmail.com>
2016-07-18 16:52:56 -0300

    pou_18/07
    projeto atualizado dia 18 de julho

--- Pou/app/src/main/java/io/github/yagoazedias/snesgamesandroid/CustomView.java
+++ Pou/app/src/main/java/io/github/yagoazedias/snesgamesandroid/CustomView.java
@@ -16,13 +16,18 @@ public class CustomView extends View implements Runnable {
     public Handler handler = new Handler();
 
     // Player properties.
-    float xpad = (float)(getPaddingLeft() + getPaddingRight()) + 300;
+    float xpad  = (float)(getPaddingLeft() + getPaddingRight()) + 300;
     float ypad = (float)(getPaddingTop() + getPaddingBottom()) + 1300;
+    float screenW;
+    float screenH;
+
+    float xplayer;
+    float yplayer;
 
     int canAnim = 2;
 
     // Water properties.
-    Rect whate = new Rect(getPaddingLeft(), (int)ypad + 300, getPaddingLeft() + 5000, (int)ypad + 2000);
+    //Rect water = new Rect((int)screenW/12, (int)screenH/3, (int)screenW, (int)screenH);
 
     Board[] board = new Board[100];
 
@@ -41,29 +46,35 @@ public class CustomView extends View implements Runnable {
 
     public CustomView(Context context) {
         super(context);
+        screenW = context.getResources().getDisplayMetrics().widthPixels;
+        screenH = context.getResources().getDisplayMetrics().heightPixels;
     }
 
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        screenW = context.getResources().getDisplayMetrics().widthPixels;
+        screenH = context.getResources().getDisplayMetrics().heightPixels;
     }
 
     public CustomView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        screenW = context.getResources().getDisplayMetrics().widthPixels;
+        screenH = context.getResources().getDisplayMetrics().heightPixels;
     }
 
     public void pouAnim() {
 
         if(canAnim == 0) {
-            ypad -= 70;
+            yplayer -= screenH/20;
 
-            for(int i = 0; i < board.length; i++) {
-                board[i].xpad -= 60;
-            }
+            //for(int i = 0; i < board.length; i++) {
+              //  board[i].xpad -= 60;
+            //}
 
         }
 
         if(canAnim == 1) {
-            ypad += 70;
+            yplayer += screenH/20;
         }
 
         if(ypad <= (getPaddingTop() + getPaddingBottom()) + 1000) {
@@ -87,18 +98,22 @@ public class CustomView extends View implements Runnable {
 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(xpad, ypad, 100, new Paint(Paint.ANTI_ALIAS_FLAG));
-        canvas.drawRect(whate, new Paint(Paint.DEV_KERN_TEXT_FLAG));
+
+        xplayer = screenW/11;
+        yplayer = screenH/2;
+
+        canvas.drawCircle(xplayer, yplayer, 100, new Paint(Paint.ANTI_ALIAS_FLAG));
+        //canvas.drawRect(screenW/30,screenH ,screenW,screenH+1, new Paint(Paint.DEV_KERN_TEXT_FLAG));
 
         pouAnim();
 
-        board[1].isVoid = true;
+       // board[1].isVoid = true;
 
-        for(int i = 0; i < board.length; i++) {
-            if(!board[i].isVoid) {
-                canvas.drawRect(board[i].xpad, board[i].ypad, board[i].xpad + 200, board[i].ypad + 200, new Paint(Paint.ANTI_ALIAS_FLAG));
-            }
-        }
+      //  for(int i = 0; i < board.length; i++) {
+      //      if(!board[i].isVoid) {
+       //         canvas.drawRect(board[i].xpad, board[i].ypad, board[i].xpad + 200, board[i].ypad + 200, new Paint(Paint.ANTI_ALIAS_FLAG));
+       //     }
+       // }
 
         try {
             Thread.sleep(30);
